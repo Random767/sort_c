b695291f4079a6018ad83b331cf6e16da4369f11
GRandom <abupilha@gmail.com>
2025-02-18 17:01:12 -0300

    Remoção de todas as funções printf comentadas para melhorar a legilibidade do código

--- main.c
+++ main.c
@@ -12,25 +12,18 @@ int main(void) {
     for(int i = 0; i < numberlist_n; i++) {
         for(int j = 0; j <numberlist_n; j++) {
             if(j < i) {
-              printf("Passando para o próximo número principal da lista...\n");
               j = i;
               continue;
             }
             if(numberlist[i] == numberlist[j]) {
-                printf("%d == %d\n", numberlist[i], numberlist[j]);
                 continue;
             }  else if(numberlist[i] > numberlist[j]) {
-                printf("%d > %d\n", numberlist[i], numberlist[j]);
                 continue;
             } else if(numberlist[i] < numberlist[j]) {
                 iterationscount++;
-                printf("%d < %d\n", numberlist[i], numberlist[j]);
                 int temp1 = numberlist[i], temp2 = numberlist[j];
                 numberlist[i] = numberlist[j];
                 numberlist[j] = temp1;
-                //printf("Trocando de posição...\n");
-                //printf("numberlist[%d] = numberlist[%d]\n", i, j);
-                //printf("numberlist[%d] = numberlist[%d]\n\n", j, i);
             }
         }
     }
